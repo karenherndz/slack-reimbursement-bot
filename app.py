796062ce28f15730ce8bb227a22a403bc23333ab
karenherndz <karen@staymarais.com>
2025-04-17 13:01:23 -0500

    Update app.py

--- app.py
+++ app.py
@@ -78,4 +78,6 @@ def weekly_summary():
     return "Summary sent", 200
 
 if __name__ == '__main__':
-    app.run(port=3000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
+
